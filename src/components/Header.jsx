3e573a95534adfaf58364c4c33f6df8fd226c27d
darkokitanoski-blip <darko.kitanoski@elev.ga.lbs.se>
2026-09-12 15:24:18 +0200

    header animation och fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ArrowIcon from "./ArrowIcon.jsx";
 
-  // komponent där jag skapar Header för sidan, och jag implementa ikon komonent och också använder useState från Reacts library
+// komponent där jag skapar Header för sidan, och jag implementerar ikon-komponenten och använder useState från React-biblioteket; 
+// här okcså gör jag animationen för header när man scrollar.
 
 const navItems = [
   ["Våra skolor", "#schools"],
@@ -12,9 +13,42 @@ const navItems = [
 
 export default function Header() {
   const [open, setOpen] = useState(false);
+  const [Transparent, setTransparent] = useState("transparent")
+  const [scrollDirection, setScrollDirection] = useState("up");
+  const [Width, setWidth] = useState("min(100% - 48px, var(--max))")
+  const [Padding, setPadding] = useState("0% 0%")
+
+  useEffect(() => {
+    let previousScrollY = window.scrollY;
+
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      setTransparent("var(--dark-cyan)")
+      setWidth("101%")
+      setPadding("0% 10%")
+      if (currentScrollY === 0) {
+        setScrollDirection("up");
+        previousScrollY = currentScrollY;
+        setTransparent("transparent")
+        setWidth("min(100% - 48px, var(--max))")
+        setPadding("0% 0%")
+        return;
+      }
+
+      setScrollDirection(currentScrollY > previousScrollY ? "down" : "up");
+      previousScrollY = currentScrollY;
+    };
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <header className="header">
+    <header
+      className="header"
+      style={{ top: scrollDirection === "down" ? "-10%" : "0%", backgroundColor: Transparent, width: Width, padding: Padding }}
+    >
       <a className="brand" href="#" aria-label="LBS Kreativa Gymnasiet">
         <span className="brand-box">
           <img src="/lbslogosvg.svg" alt="LBS" />
